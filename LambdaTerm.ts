e89515364e7c19d54d363d0250749249ff23e984
stevenhuyn <18359644+stevenhuyn@users.noreply.github.com>
2024-01-03 20:12:46 -0800

    Add further attempts to Lambda Calculus

--- LambdaTerm.ts
+++ LambdaTerm.ts
@@ -1,33 +1,70 @@
+import { AgentId, AgentKind, INet, Port, Slot } from "./INet";
+
 interface LamExpr {
   kind: "Lambda";
-  body: Expr;
+  arg: Symbol;
+  body: LambdaExpr;
 }
 
 interface AppExpr {
   kind: "Application";
-  left: Expr;
-  right: Expr;
+  left: LambdaExpr;
+  right: LambdaExpr;
 }
 
 interface VarExpr {
   kind: "Variable";
-  name: string;
+  name: Symbol;
 }
 
-const LamExpr = (body: Expr): LamExpr => ({
+interface ArgExpr {
+  kind: "Argument";
+  name: Symbol;
+}
+
+const LamExpr = (arg: Symbol, body: LambdaExpr): LamExpr => ({
   kind: "Lambda",
+  arg,
   body,
 });
 
-const AppExpr = (left: Expr, right: Expr): AppExpr => ({
+const AppExpr = (left: LambdaExpr, right: LambdaExpr): AppExpr => ({
   kind: "Application",
   left,
   right,
 });
 
-const VarExpr = (name: string): VarExpr => ({
-  kind: "Variable",
-  name,
-});
+const VarExpr = (name: Symbol): VarExpr => {
+  return {
+    kind: "Variable",
+    name: name,
+  };
+};
 
-type Expr = LamExpr | AppExpr | VarExpr;
+type LambdaExpr = LamExpr | AppExpr | VarExpr;
+
+const lambdaToNet = (expr: LambdaExpr): INet => {
+  // Holding expression + parent to relink
+  let stack: [LambdaExpr, AgentId][] = [[expr, 0]];
+  let net = INet.default();
+  let args: Set<Symbol> = [];
+
+  while (stack.length > 0) {
+    const [currLamExpr, parentNetId] = stack.pop()!;
+
+    if (currLamExpr.kind == "Lambda") {
+      const arg = currLamExpr.arg;
+      args.add(arg);
+      const currNetId = net.alloc(AgentKind.Con);
+
+      const parentType = net.nodes[parentNetId].kind;
+      if (parentType == AgentKind.Con) {
+        net.link(Port.prin(currNetId), Port.right(parentNetId));
+      } else if (parentType == AgentKind.Con) {
+        net.link(Port.prin(currNetId), Port.left(parentNetId));
+      }
+
+      stack.push([currLamExpr.body, currNetId]);
+    }
+  }
+};
